9eebe8d32faac9b7e2f31fd67bc873fd4ee9e973
Vo1z <vitya_voody@ukr.net>
2020-09-26 02:27:43 +0300

    Super fast working RUST calculator <3 <3 <3

--- src/job/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod eval;
-pub mod lex;
-pub mod parse;
-
-// pub use eval::*;
-// pub use lex::*;
-// pub use parse::*;
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use iced::Settings;
-use iced::Application;
-
-mod app;
-use crate::app::*;
-
-fn main() {
-    Calculator::run(Settings::default());
-}
