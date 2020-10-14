01cee8e732e3173611abb89b39b4f983ff637af4
tema3210 <play.aab.25122001@gmail.com>
2020-10-15 00:38:52 +0300

    clear parse local, commit trash to its file; implement token promotion

--- src/job/parse.rs
+++ src/job/parse.rs
@@ -6,6 +6,47 @@ pub(crate) fn parse(tokens: Vec<Token>) -> Result<TreeNode,AppError> {
 }
 
 
+#[derive(Debug)]
+enum TokenExtended {
+    Tok(Token),
+    Node(TreeNode),
+}
+
+fn promote_to_extended_tokens(toks: Vec<Token>,parse_fn: impl Fn(&[Token])->Result<TreeNode,AppError>) -> Result<Vec<TokenExtended>,AppError> {
+    //of braces
+    let ranges = process_braces(&toks)?;
+    let mut ret = Vec::new();
+
+    //current range of braces
+    let mut curr_indice = 0;
+    //was it aleady inserted
+    let mut flag = false;
+
+    for i in 0..toks.len() {
+        let (left,right) = ranges[curr_indice];
+
+        if i >= left && i <= right {
+            if !flag {
+                let l = left + 1;
+                let r = right - 1;
+                ret.push(TokenExtended::Node(parse_fn(&toks[l..=r])?));
+                flag = true;
+            }
+        } else {
+            if flag {
+                curr_indice+=1;
+                flag = false;
+            };
+            ret.push(TokenExtended::Tok(toks[i]));
+        };
+
+    };
+
+    ret.retain(|el| !matches!(el,TokenExtended::Tok(Token::Brace{lhs: _})));
+    Ok(ret)
+}
+
+
 fn process_braces(toks: &[Token]) -> Result<Vec<(usize,usize)>,AppError> {
     let mut res: (Vec<_>,isize,(Option<usize>,Option<usize>)) = toks.iter().enumerate().try_fold(
         (Vec::new(),0isize,(None,None)),
@@ -47,8 +88,7 @@ fn process_braces(toks: &[Token]) -> Result<Vec<(usize,usize)>,AppError> {
                 },
             };
             Ok((vec,acc,curr_interval))
-    }
-    )?;
+    })?;
     if res.1 == 0 {
         if let (Some(lhs),Some(rhs)) = res.2 {
             res.0.push((lhs,rhs));
@@ -60,7 +100,7 @@ fn process_braces(toks: &[Token]) -> Result<Vec<(usize,usize)>,AppError> {
 }
 
 #[cfg(test)]
-mod parser_tests {
+mod braces_tests {
     use crate::job as T;
 
     //This is helper
@@ -96,8 +136,13 @@ mod parser_tests {
     }
     #[test]
     fn t_04() {
-        let toks = T::lex::lexer("( 10 - 3 ) * 17 + (11-2) / (12 - 7)").unwrap();
+        let toks = T::lex::lexer("( 10 - 3 ) * 17 + (11+-2) / (12 - 7)").unwrap();
         let br_res = T::parse::process_braces(&toks).unwrap();
         assert_eq!(br_res, vec![(0,4),(8,12),(14,18)]);
     }
+}
+
+#[cfg(test)]
+mod promotion_tests {
+
 }
